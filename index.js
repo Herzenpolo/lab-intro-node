8861fa6b25f6ed96a1592803b8db31712e17d456
Herzen Cortes <herzencc@icloud.com>
2020-05-07 16:21:46 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+ add(item) {
+      this.items.push(item)
+      this.items.sort((a,b) => a-b)
+      this.length = this.items.length
+    }
+  
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] === undefined) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0){
+      throw new Error ('EmptySortedList')
+    } else {
+      let arr1 = this.items
+      return Math.max(...arr1)
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0){
+      throw new Error ('EmptySortedList')
+    } else {
+      let arr1 = this.items
+      return Math.min(...arr1)
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0){
+      return 0
+    } else {
+      return this.items.reduce((a,b) => a+b)
+  }
+}
 
-  avg() {}
+  avg() {
+    if (this.length === 0){
+      throw new Error ('EmptySortedList')
+    } else {
+      return this.sum()/this.length
+  }
+  }
 }
 
 module.exports = SortedList;
